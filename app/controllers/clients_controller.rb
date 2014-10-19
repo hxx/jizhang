2128e701ccad55388078916bce294ecf4b8b1281
Star Huang <star@beansmile.com>
2014-10-19 10:39:59 +0800

    config for devise

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,6 @@
 class ClientsController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @clients = @user.clients.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :clients
-  end
+  devise_for :users
+
+  resources :clients
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'clients#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
