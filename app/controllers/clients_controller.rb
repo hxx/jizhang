2325d3a5a34fb0c33770653ed3736d7c399a0462
Star Huang <star@beansmile.com>
2014-10-19 11:18:02 +0800

    CRUD for clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,53 @@
 class ClientsController < ApplicationController
+  before_action :set_user
+  before_action :set_client, only: [:show, :edit, :update, :destroy]
+
   def index
-    @user = User.find(current_user.id)
     @clients = @user.clients.all
+  end
+
+  def show
+  end
+
+  def new
+    @client = @user.clients.new
+  end
+
+  def create
+    if @client = @user.clients.create(client_params)
+      redirect_to @client
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @client.update(client_params)
+      redirect_to @client
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @client.destroy
+    redirect_to clients_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def set_client
+    @client = @user.clients.find(params[:id])
+  end
+
+  def client_params
+    params.require(:client).permit(:name, :content)
   end
 end
